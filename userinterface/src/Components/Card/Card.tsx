5e0c00978168a0a78cf9f5b2e36d189f64806285
Rafael <arafaelsmoreira@gmail.com>
2026-04-07 17:47:51 +0100

    Create Card component and basic card list layout

---
+++ userinterface/src/Components/Card/Card.tsx
@@ -0,0 +1,22 @@
+import React, { JSX } from 'react'
+import "./Card.css"
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
+
+const Card: React.FC<Props> = ({companyName, ticker, price}  : Props) : JSX.Element => {
+  return (
+    <div className='card'>
+        <div className='details'>
+            <h2>{companyName} ({ticker})</h2>
+            <p>${price}</p>
+        </div>
+        <p className='info'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, praesentium!
+        </p>
+    </div>
+  )
+}
+export default Card
